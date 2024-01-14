65da3fa9292a637ea3dcb48b9c7060e8b818a02f
Zhi <zhichen0428@gmail.com>
2024-01-14 15:34:20 -0500

    untrack unncessary files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,7 +5,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(eglot xclip))
+ '(warning-suppress-types '((emacs))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -74,3 +76,5 @@
 ;; enable copy and paste to clipboard in emacs nw
 ;; Remember to install xclip for this
 (xclip-mode 1)
+
+;; Remember to install eglot for this
